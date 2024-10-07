a6cba8f83ca393f2b97d837a7c944c0bd9f0df4b
Atul Adhikari <atuladhikari01@gmail.com>
2024-10-07 18:12:42 +0545

    Add components folder for code reusability

---
+++ eCommerce/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="">Home</Link>
+      <Link to="">My Cart</Link>
+    </div>
+  )
+}
+
+export default Navbar
