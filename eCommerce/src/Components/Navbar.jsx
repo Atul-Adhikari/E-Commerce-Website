14f9d472bcc07ce2326e38d8e8455826266a24e4
Atul Adhikari <atuladhikari01@gmail.com>
2024-10-07 18:38:40 +0545

    Add style to navbar

--- eCommerce/src/Components/Navbar.jsx
+++ eCommerce/src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import styles from '../Styles/Navbar.module.css'
+import {ShoppingCart} from 'phosphor-react'
+import {HouseLine} from 'phosphor-react'
 const Navbar = () => {
   return (
-    <div>
-      <Link to="">Home</Link>
-      <Link to="">My Cart</Link>
+    <div className={styles.navbar}>
+      <Link className={styles.links} to="/"><HouseLine/> Home</Link>
+      <Link className={styles.links} to="/cart"><ShoppingCart/> My Cart</Link>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
