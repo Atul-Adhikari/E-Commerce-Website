f3096b978fbf9d00454052be71f4e8afa9100cd3
Atul Adhikari <atuladhikari01@gmail.com>
2024-12-05 18:48:59 +0545

    Changes in css for better UI and added favorites icon

--- eCommerce/src/Components/Items.jsx
+++ eCommerce/src/Components/Items.jsx
@@ -2,13 +2,22 @@ import React, { useContext, useState } from "react";
 import { MyContext } from "../App";
 import styles from "../Styles/Items.module.css";
 import { ShoppingCart } from "phosphor-react";
+import { MdFavorite } from "react-icons/md";
+import { MdFavoriteBorder } from "react-icons/md";
+
 const Items = ({ product }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const changeFavoriteStatus=()=>{
+    setIsFavorite(!isFavorite);
+  }
   const { products, setProducts, carts, setCarts } = useContext(MyContext);
   const saveToCart = () => {
     if (carts.includes(product)) {
       alert(product.title + "already in cart.");
       return;
     }
+    alert("Item added to card.");
     const updatedCarts = [...carts, product];
     setCarts(updatedCarts);
     localStorage.setItem("carItem", JSON.stringify(updatedCarts));
@@ -28,9 +37,22 @@ const Items = ({ product }) => {
           />
           <p className={styles.productName}>{product.title}</p>
           <p className={styles.productPrice}>$ {product.price}</p>
-          <button className={styles.addToCart} onClick={saveToCart}>
-            Add to Cart <ShoppingCart size={20} />
-          </button>
+          <div className={styles.buttons}>
+            <button className={styles.addToCart} onClick={saveToCart}>
+              Add to Cart <ShoppingCart size={20} />
+            </button>
+            <div className={styles.favorites} onClick={changeFavoriteStatus}>
+              {isFavorite ? (
+                <div>
+                  <MdFavorite size={18}/>
+                </div>
+              ) : (
+                <div>
+                  <MdFavoriteBorder size={18}/>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- eCommerce/src/Components/Navbar.jsx
+++ eCommerce/src/Components/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from '../Styles/Navbar.module.css'
-import {ShoppingCart} from 'phosphor-react'
-import {HouseLine} from 'phosphor-react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../Styles/Navbar.module.css";
+import { ShoppingCart } from "phosphor-react";
+import { HouseLine } from "phosphor-react";
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-      <Link className={styles.links} to="/"><HouseLine/> Home</Link>
-      <Link className={styles.links} to="/cart"><ShoppingCart/> My Cart</Link>
+    <div className="">
+      <div className={styles.navbar}>
+        <Link className={styles.links} to="/">
+          <HouseLine /> Home
+        </Link>
+        <Link className={styles.links} to="/cart">
+          <ShoppingCart /> My Cart
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
